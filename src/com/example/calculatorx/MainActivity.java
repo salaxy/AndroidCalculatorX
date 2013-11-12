82df8af86191e14ae2a85c9629eddbf06d37b8ca
Salaxy <salaxy@web.de>
2013-11-12 20:53:58 +0100

    cleaning code, adding better error handling, stable version

--- src/com/example/calculatorx/MainActivity.java
+++ src/com/example/calculatorx/MainActivity.java
@@ -16,32 +16,16 @@ import android.widget.TextView;
  */
 public class MainActivity extends Activity implements OnClickListener {
 
-	private Rechner logic;
-
-	private Button button0;
-	private Button button1;
-	private Button button2;
-	private Button button3;
-	private Button button4;
-	private Button button5;
-	private Button button6;
-	private Button button7;
-	private Button button8;
-	private Button button9;
-	private Button buttonPlus;
-	private Button buttonMinus;
-	private Button buttonDiv;
-	private Button buttonMult;
-	private Button buttonC;
-	private Button buttonEqual;
+	private Rechner logic;	
 	private TextView display;
-	private View rootView;
 
 	private Double result;
 	public Double secoundValue;
 	public Double firstValue;
+	
 	private String lastOperation = null;
-	private boolean inputOperation = false;
+	private boolean operationWasPressed = false;
+	private boolean errorState=false;
 
 	private final String FAIL_NOTIFICATION = "error";
 	private final int MAX_NUMS=9;
@@ -72,28 +56,18 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		if (view instanceof Button) {
 			String input = ((Button) view).getText().toString();
-			Log.e("pressed: ", input);
+			Log.i("pressed: ", input);
 
-			if (display.getText().equals(FAIL_NOTIFICATION)) {
-
+			if (errorState) {
 				if (input.equals("C")) {
-					display.setText("0");
-					inputOperation = false;
+					setBackValues(input);
 				}
-
-			} else if (input.equals("0")) {
-				display.append(input);
-
-				// prevent leading nulls
-				if (Double.parseDouble(display.getText().toString()) == 0.0d) {
-					display.setText("0");
-				}
-
+				Log.i("error notification detected", input);
 			} else if (input.equals("1") || input.equals("2")
 					|| input.equals("3") || input.equals("4")
 					|| input.equals("5") || input.equals("6")
 					|| input.equals("7") || input.equals("8")
-					|| input.equals("9")) {
+					|| input.equals("9")|| input.equals("0")) {
 
 				// prevent leading nulls
 				if (Double.parseDouble(display.getText().toString()) == 0.0d) {
@@ -101,12 +75,13 @@ public class MainActivity extends Activity implements OnClickListener {
 				}
 
 				// set back display if operation was pressed
-				if (inputOperation) {
+				if (operationWasPressed) {
 					display.setText("");
 					display.append(input);
-					inputOperation = false;
+					operationWasPressed = false;
 				} else {
 					display.append(input);
+					Log.i("append", input);
 				}
 
 			} else if (input.equals("=")) {
@@ -115,62 +90,76 @@ public class MainActivity extends Activity implements OnClickListener {
 				if (lastOperation != null) {
 					secoundValue = Double.parseDouble(display.getText()
 							.toString());
+					Log.i("secoundValue",secoundValue.toString());
 
-					if (lastOperation.equals("*")) {
-						result = logic.mul(firstValue, secoundValue);
-						Log.e("blubb", "mult");
-					} else if (lastOperation.equals("/")) {
-						result = logic.div(firstValue, secoundValue);
-						Log.e("blubb", "div");
-					} else if (lastOperation.equals("-")) {
-						result = logic.sub(firstValue, secoundValue);
-					} else if (lastOperation.equals("+")) {
-						result = logic.add(firstValue, secoundValue);
-					}
+					calcResult();
 
+					//catch exceptions
 					if (String.valueOf(result).length() > MAX_NUMS || result.isInfinite() ||result.isNaN()) {
 						display.setText(FAIL_NOTIFICATION);
+						errorState=true;
 					} else {
 						display.setText(result.toString());
-						Log.e("blubb", result.toString());
+						Log.i("result", result.toString());
 					}
 
 					lastOperation = null;
 				}
 			} else if (input.equals("/") || input.equals("*")
 					|| input.equals("+") || input.equals("-")) {
+				
 				firstValue = Double.parseDouble(display.getText().toString());
-				Log.e("blubb", display.getText().toString());
+				
+				Log.i("operation pressed", input);
+				Log.i("firstValue",firstValue.toString());
 
-				lastOperation = input.toString();
-				inputOperation = true;
+				lastOperation = input;
+				operationWasPressed = true;
 			} else if (input.equals("C")) {
-				display.setText("0");
-				inputOperation = false;
+				setBackValues(input);
 			}
 
 		}
 	}
 
+	/**
+	 * calculate the result Value with the last operation
+	 */
+	private void calcResult() {
+		if (lastOperation.equals("*")) {
+			result = logic.mul(firstValue, secoundValue);
+			Log.i("calculate", "mult");
+		} else if (lastOperation.equals("/")) {
+			try{
+				result = logic.div(firstValue, secoundValue);
+				Log.i("calculate", "div");
+			}catch(ArithmeticException e){
+				result=Double.NaN;
+			}
+		} else if (lastOperation.equals("-")) {
+			result = logic.sub(firstValue, secoundValue);
+			Log.i("calculate", "sub");
+		} else if (lastOperation.equals("+")) {
+			result = logic.add(firstValue, secoundValue);
+			Log.i("calculate", "add");
+		}
+	}
+
+	/**
+	 * set Back all Values
+	 * @param input - last input
+	 */
+	private void setBackValues(String input) {
+		display.setText("0");
+		operationWasPressed = false;
+		lastOperation=null;
+		firstValue=0.0d;
+		secoundValue=0.0d;
+		errorState=false;
+		Log.i("setback pressed", input);
+	}
+
 	private void initViews() {
-		button0 = (Button) findViewById(R.id.button0);
-		button1 = (Button) findViewById(R.id.button1);
-		button2 = (Button) findViewById(R.id.button2);
-		button3 = (Button) findViewById(R.id.button3);
-		button4 = (Button) findViewById(R.id.button4);
-		button5 = (Button) findViewById(R.id.button5);
-		button6 = (Button) findViewById(R.id.button6);
-		button7 = (Button) findViewById(R.id.button7);
-		button8 = (Button) findViewById(R.id.button8);
-		button9 = (Button) findViewById(R.id.button9);
-
-		buttonPlus = (Button) findViewById(R.id.buttonPlus);
-		buttonMinus = (Button) findViewById(R.id.buttonMinus);
-		buttonDiv = (Button) findViewById(R.id.buttonDiv);
-		buttonMult = (Button) findViewById(R.id.buttonMult);
-
-		buttonC = (Button) findViewById(R.id.buttonC);
-		buttonEqual = (Button) findViewById(R.id.buttonEqual);
 
 		display = (TextView) findViewById(R.id.output);
 	}
