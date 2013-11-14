f6e8148e0cf9c18be8e6db5ac45caa496eb0b453
Salaxy <salaxy@web.de>
2013-11-14 18:05:08 +0100

    optmize Sourcecode, different onClick-Methods

--- src/com/example/calculatorx/MainActivity.java
+++ src/com/example/calculatorx/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.calculatorx;
 
 import java.text.DecimalFormat;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,120 +14,150 @@ import android.widget.TextView;
  * @author Andy Klay <klay@fh-brandenburg.de>
  * 
  */
-public class MainActivity extends Activity implements OnClickListener {
-
-	private Rechner logic;	
+public class MainActivity extends Activity {
+	
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button buttonPlus;
+    private Button buttonMinus;
+    private Button buttonDiv;
+    private Button buttonMult;
+    private Button buttonC;
+    private Button buttonEqual;
+	
+    
+	private Rechner logic;
 	private TextView display;
 
 	private Double result;
 	public Double secoundValue;
 	public Double firstValue;
-	
+
 	private String lastOperation = null;
 	private boolean operationWasPressed = false;
-	private boolean errorState=false;
-	
+	private boolean errorState = false;
 	private boolean firstValueTypedIn = false;
 
 	private final String FAIL_NOTIFICATION = "error";
-	private final int MAX_NUMS=9;
+	private final int MAX_NUMS = 9;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-
 		initViews();
 		logic = new Rechner();
 		firstValue = 0.0d;
 		secoundValue = 0.0d;
 		result = 0.0d;
-
-		return true;
 	}
 
-	@SuppressLint("NewApi")
-	@Override
-	public void onClick(View view) {
+//	@Override
+//	public void onClick(View view) {
+//
+//		if (view instanceof Button) {
+//			String input = ((Button) view).getText().toString();
+//			Log.i("pressed: ", input);
+//
+//			analyzeInput(input);
+//		}
+//	}
 
-		if (view instanceof Button) {
-			String input = ((Button) view).getText().toString();
-			Log.i("pressed: ", input);
-			
-			analyzeInput(input);
+	public void onClickNumber(View view) {
+
+		String input = ((Button) view).getText().toString();
+		if (!errorState) {
+			addingNewNumberToDisplay(((Button) view).getText().toString());
 		}
 	}
 
-	/**
-	 * Analyzing button input
-	 * @param input - String name of clicked Button
-	 */
-	private void analyzeInput(String input) {
-		
-		if (errorState) {
-			allowPressingSetBackButton(input);
-		} else if (isNumber(input)) {
-			addingNewNumberToDisplay(input);
-		} else if (isEquals(input)) {
+//	/**
+//	 * Analyzing button input
+//	 * 
+//	 * @param input
+//	 *            - String name of clicked Button
+//	 */
+//	private void analyzeInput(String input) {
+//
+//		if (errorState) {
+//			allowPressingSetBackButton(input);
+//		} else if (isNumber(input)) {
+//			addingNewNumberToDisplay(input);
+//		} else if (isEquals(input)) {
+//			onClickEquals();
+//		} else if (isOperator(input)) {
+//			onClickOperator(input);
+//		} else if (input.equals("C")) {
+//			onClickCancel(input);
+//		}
+//	}
+
+	
+	public void onClickCancel(View view) {
+		String input = ((Button) view).getText().toString();
+		setBackValues(input);
+	}
+
+	public void onClickEquals(View view) {
+		String input = ((Button) view).getText().toString();
+		if (!errorState) {
 			if (lastOperation != null) {
 				readInSecoundValueFromDisplay();
 				calcResult();
 				displayResult();
 				lastOperation = null;
-				operationWasPressed=false;
-				firstValueTypedIn=true;
+				operationWasPressed = false;
+				firstValueTypedIn = true;
 			}
-		} else if (isOperator(input)) {
+		}
+	}
+
+	public void onClickOperator(View view) {
+String input = ((Button) view).getText().toString();
+
+		if (!errorState) {
 			Log.i("operation pressed", input);
 			lastOperation = input;
 			operationWasPressed = true;
-			
-			if(firstValueTypedIn){
-				Log.i("chaining","operator pressed");
-				
+
+			if (firstValueTypedIn) {
+				Log.i("chaining", "operator pressed");
+
 				readInSecoundValueFromDisplay();
 				calcResult();
 				displayResult();
-				firstValueTypedIn=true; 
-			}else{
+				firstValueTypedIn = true;
+			} else {
 				readInFirstValueFromDisplay();
 			}
-		} else if (input.equals("C")) {
-			setBackValues(input);
 		}
 	}
 
-	private boolean isEquals(String input) {
-		return input.equals("=");
-	}
+//	private boolean isEquals(String input) {
+//		return input.equals("=");
+//	}
+//
+//	private boolean isOperator(String input) {
+//		return input.equals("/") || input.equals("*") || input.equals("+")
+//				|| input.equals("-");
+//	}
 
-	private boolean isOperator(String input) {
-		return input.equals("/") || input.equals("*")
-				|| input.equals("+") || input.equals("-");
-	}
-
-	private boolean isNumber(String input) {
-		return input.equals("1") || input.equals("2")
-				|| input.equals("3") || input.equals("4")
-				|| input.equals("5") || input.equals("6")
-				|| input.equals("7") || input.equals("8")
-				|| input.equals("9")|| input.equals("0");
-	}
-
-	private void allowPressingSetBackButton(String input) {
-		if (input.equals("C")) {
-			setBackValues(input);
-		}else{
-			Log.i("input blocked through error state", input);			
-		}
-	}
+//	private void allowPressingSetBackButton(String input) {
+//		if (input.equals("C")) {
+//			onClickCancel(input);
+//		} else {
+//			Log.i("input blocked through error state", input);
+//		}
+//	}
 
 	private void addingNewNumberToDisplay(String input) {
 		preventTypedinLeadingNulls();
@@ -154,7 +181,7 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	private void readInSecoundValueFromDisplay() {
 		secoundValue = Double.parseDouble(display.getText().toString());
-		Log.i("readInSecoundValueFromDisplay",secoundValue.toString());
+		Log.i("readInSecoundValueFromDisplay", secoundValue.toString());
 	}
 
 	private void readInFirstValueFromDisplay() {
@@ -163,15 +190,16 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	private void displayResult() {
-		//cut long digits after decimal point etc
-		if(!result.isInfinite() &&!result.isNaN()){
-			restrictResultToNineSigns();		
+		// cut long digits after decimal point etc
+		if (!result.isInfinite() && !result.isNaN()) {
+			restrictResultToNineSigns();
 		}
 
-		//catch exceptions
-		if (String.valueOf(result).length() > MAX_NUMS || result.isInfinite() ||result.isNaN()) {
+		// catch exceptions
+		if (String.valueOf(result).length() > MAX_NUMS || result.isInfinite()
+				|| result.isNaN()) {
 			display.setText(FAIL_NOTIFICATION);
-			errorState=true;
+			errorState = true;
 		} else {
 			display.setText(result.toString());
 			Log.i("display result", result.toString());
@@ -179,12 +207,14 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	private void restrictResultToNineSigns() {
-		//TODO Problem mit der E-Schreibweise treten immer wieder auf und führen zu Error-Meldungen
+		// TODO Problem mit der E-Schreibweise treten immer wieder auf und
+		// führen zu Error-Meldungen
 		DecimalFormat formatter = new DecimalFormat("#.##");
-		result=Double.parseDouble(formatter.format(result));
-		Double roundedResult = Math.round(result*10000000) / 10000000.0;
-		Log.i("formated result",roundedResult.toString());
-		result=roundedResult;
+		// Kurz vor der ausagbe nochmal formatter benutzen
+		result = Double.parseDouble(formatter.format(result));
+		Double roundedResult = Math.round(result * 10000000) / 10000000.0;
+		Log.i("formated result", roundedResult.toString());
+		result = roundedResult;
 	}
 
 	/**
@@ -195,11 +225,11 @@ public class MainActivity extends Activity implements OnClickListener {
 			result = logic.mul(firstValue, secoundValue);
 			Log.i("calculate", firstValue + " mult " + secoundValue);
 		} else if (lastOperation.equals("/")) {
-			try{
+			try {
 				result = logic.div(firstValue, secoundValue);
-				Log.i("calculate",firstValue + " div " + secoundValue);
-			}catch(ArithmeticException e){
-				result=Double.NaN;
+				Log.i("calculate", firstValue + " div " + secoundValue);
+			} catch (ArithmeticException e) {
+				result = Double.NaN;
 			}
 		} else if (lastOperation.equals("-")) {
 			result = logic.sub(firstValue, secoundValue);
@@ -209,27 +239,49 @@ public class MainActivity extends Activity implements OnClickListener {
 			Log.i("calculate", firstValue + " add " + secoundValue);
 		}
 		Log.i("result", result.toString());
-		
-		//setze result als  firstValue um gleich weiter rechnen zu können
-		//TODO kritische Stelle, intern wird mit dem result als firstValue weitergerechnet
-		firstValue=result;
+
+		// setze result als firstValue um gleich weiter rechnen zu können
+		// TODO kritische Stelle, intern wird mit dem result als firstValue
+		// weitergerechnet
+		firstValue = result;
 	}
 
 	/**
 	 * set Back all Values
-	 * @param input - last input
+	 * 
+	 * @param input
+	 *            - last input
 	 */
 	private void setBackValues(String input) {
 		display.setText("0");
 		operationWasPressed = false;
-		lastOperation=null;
-		firstValue=0.0d;
-		secoundValue=0.0d;
-		errorState=false;
+		lastOperation = null;
+		firstValue = 0.0d;
+		secoundValue = 0.0d;
+		errorState = false;
 		Log.i("setback pressed", input);
 	}
 
 	private void initViews() {
+		
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        buttonDiv = (Button) findViewById(R.id.buttonDiv);
+        buttonMult = (Button) findViewById(R.id.buttonMult);
+        buttonEqual = (Button) findViewById(R.id.buttonEqual);       
+        
+        buttonC = (Button) findViewById(R.id.buttonC);
+		
 		display = (TextView) findViewById(R.id.output);
 	}
 
